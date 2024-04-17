88693abd76a612f1129d89be4dd1ca5d3af5d31e
James Okada <jyo1291@gmail.com>
2024-04-17 00:26:42 -0700

    fix build

--- app/components/cards/leagueInfoCard.tsx
+++ app/components/cards/leagueInfoCard.tsx
@@ -6,16 +6,19 @@ interface LeagueInfoCardProps {
   profileData?: LeagueProfileData;
 }
 const LeagueInfoCard: React.FC<LeagueInfoCardProps> = ({ profileData }) => {
+  if (!profileData) {
+    return null;
+  }
   return (
     <div className="flex flex-col items-center rounded shadow-lg bg-white p-3 px-8 curs m-3">
       <Image
-        src={profileData?.profileIconPath}
+        src={profileData.profileIconPath}
         alt="James Cover photo"
         width="75"
         height="75"
       />
-      <h2 className="text-xl">{profileData?.riotId}</h2>
-      <h2 className="text-xl">Summoner Level : {profileData?.summonerLevel}</h2>
+      <h2 className="text-xl">{profileData.riotId}</h2>
+      <h2 className="text-xl">Summoner Level : {profileData.summonerLevel}</h2>
     </div>
   );
 };
